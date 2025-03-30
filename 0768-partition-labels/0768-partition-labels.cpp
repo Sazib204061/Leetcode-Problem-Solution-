2ea4cb26b102004d504df46ca1a53a5e40dcb691
Md Sazibur Rahman <113656322+Sazib204061@users.noreply.github.com>
2025-03-30 10:45:04 +0600

    Time: 2 ms (40.51%), Space: 8.9 MB (49.45%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        map<char, int>lastOccuranceIndex;
+        int n = s.size();
+        for(int i = 0; i<n; i++)
+        {
+            lastOccuranceIndex[s[i]] = i;
+        }
+        int L = 0, R = lastOccuranceIndex[s[L]], startIndex = 0;
+        vector<int>ans;
+        while(1)
+        {
+            if(R==n-1)
+            {
+                ans.push_back(R-startIndex+1);
+                return ans;
+            }
+            if(L==R)
+            {
+                ans.push_back(R-startIndex+1);
+                L = R+1;
+                R = lastOccuranceIndex[s[L]];
+                startIndex = L;
+            }
+            else
+            {
+                R = max(R, lastOccuranceIndex[s[L]]);
+                L++;
+            }
+
+        }
+
+        return ans;
+
+    }
+};
