10519063cc069536e28c2dccc3a22aa4265f8eb6
Md Sazibur Rahman <113656322+Sazib204061@users.noreply.github.com>
2025-04-18 16:38:35 +0600

    Time: 37 ms (15.02%), Space: 14.9 MB (17.06%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
+    vector<string>ar;
+    void cal()
+    {
+        ar.push_back("1");
+
+        for(int i = 1; i<=30; i++)
+        {
+            string s = ar[i-1], ans = "", sub = "";
+            sub+=s[0];
+            int occ = 0;
+            for(int j = 1; j<s.size(); j++)
+            {
+                //cout<<sub<<endl;
+                if(s[j]==sub[0])
+                {
+                    sub+=s[j];
+                }
+                else
+                {
+                    occ = sub.size();
+                    ans+=to_string(occ)+sub[0];
+                    sub = "";
+                    sub += s[j];
+                    //cout<<"ans = "<<ans<<endl;
+                }
+            }
+
+            occ = sub.size();
+            ans+=to_string(occ)+sub[0];
+            sub = "";
+            ar.push_back(ans);
+        }
+    }
     string countAndSay(int n) {
-        
+        cal();
+        return ar[n-1];
     }
 };
