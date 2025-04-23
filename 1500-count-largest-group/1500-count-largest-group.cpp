b6df72a723e97388b66293d3b23ead6a93a9d72d
Md Sazibur Rahman <113656322+Sazib204061@users.noreply.github.com>
2025-04-24 00:31:26 +0600

    Time: 3 ms (33.9%), Space: 7.9 MB (74.92%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int countLargestGroup(int n) {
+        map<int,int>mp;
+        int mx = 0;
+        for(int i = 1; i<=n; i++)
+        {
+            int nn = i, sum = 0;
+            while(nn)
+            {
+                sum+=(nn%10);
+                nn/=10;
+            }
+            mp[sum]++;
+            mx = max(mx, mp[sum]);
+        }
+        int ans = 0;
+        for(auto it : mp)
+        {
+            if(it.second==mx)ans++;
+        }
+        return ans;
+    }
+};
