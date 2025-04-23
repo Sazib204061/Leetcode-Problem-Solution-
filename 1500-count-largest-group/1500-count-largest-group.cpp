d492261201b3290d329b05351f1d66afe4726662
Md Sazibur Rahman <113656322+Sazib204061@users.noreply.github.com>
2025-04-24 00:31:49 +0600

    Time: 4 ms (13.22%), Space: 8.2 MB (44.07%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.cpp
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     int countLargestGroup(int n) {
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         int mx = 0;
         for(int i = 1; i<=n; i++)
         {
