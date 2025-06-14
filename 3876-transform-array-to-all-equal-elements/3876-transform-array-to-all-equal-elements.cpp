3995230589d81d879492e62f73a5180a910c7089
Md Sazibur Rahman <113656322+Sazib204061@users.noreply.github.com>
2025-06-15 00:25:39 +0600

    Time: 8 ms (84.56%), Space: 158.1 MB (82.75%) - LeetHub

---
+++ 3876-transform-array-to-all-equal-elements/3876-transform-array-to-all-equal-elements.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool canMakeEqual(vector<int>& nums, int k) {
+        int n = nums.size(), K = k;
+        bool ans = true;
+
+        vector<int>nums1 = nums;
+
+        for(int i = 0; i<n; i++)
+        {
+            if(nums[i]==-1)
+            {
+                if(i==n-1 || k<=0)
+                {
+                    ans = false;
+                    break;
+                }
+                else
+                {
+                    nums[i]*=-1;
+                    nums[i+1]*=-1;
+                    k--;
+                }
+            }
+        }
+
+        if(ans)
+        {
+            cout<<"1"<<endl;
+            return ans;
+        }
+        ans = true;
+        k = K;
+        for(int i = 0; i<n; i++)
+        {
+            if(nums1[i]==1)
+            {
+                if(i==n-1 || k<=0)
+                {
+                    ans = false;
+                    break;
+                }
+                else
+                {
+                    nums1[i]*=-1;
+                    nums1[i+1]*=-1;
+                    k--;
+                }
+            }
+        }
+        
+
+
+        
+        return ans;
+    }
+};
